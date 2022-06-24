3d7b7d23fdd5b81e5d942d9634dc4c0fb6acc4fa
bemrekalkan <bemre.kalkan@gmail.com>
2022-06-24 23:09:24 +0300

    setTimeout Func. will be used.

--- app.js
+++ app.js
@@ -2,3 +2,11 @@
 let timer = document.querySelector(".timer");
 let starter = document.querySelector(".starter");
 let reset = document.querySelector(".reset");
+let hour = document.querySelector(".hour");
+let minute = document.querySelector(".minute");
+let second = document.querySelector(".second");
+let milisecond = document.querySelector(".milisecond");
+
+setTimeout(time, 100);
+
+function time() {}
